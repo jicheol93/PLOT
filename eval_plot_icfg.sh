5c75bd535bef94dcd057f5da9740d02112f1d94b
jicheol93 <39580015+jicheol93@users.noreply.github.com>
2025-01-28 22:13:53 +0900

    Update eval_plot_icfg.sh

--- eval_plot_icfg.sh
+++ eval_plot_icfg.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-CUDA_VISIBLE_DEVICES=5 \ 
+CUDA_VISIBLE_DEVICES=0 \ 
 python test_plot.py \
---config_file ./logs/ICFG-PEDES/20240229_normID_TextAggPredWoM2M_Temp0015_5_15_AmpScaler_SharedLocalSlot8TextAttnMask_ReconAllScale001SDMCLScale5_slotLr5_LearnableSlotsIter5/configs.yaml \
-#--config_file ./logs/ICFG-PEDES/plot/configs.yaml \
+--config_file ./logs/ICFG-PEDES/plot/configs.yaml \
